4ee20f25470b71eab0ccadcb52e29a7c40b4496e
lfwang-sjtu <wanglefeng@sjtu.edu.cn>
2024-03-31 01:02:55 +0800

    environment configuration

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # HuggingfaceData
+
+### 启动所有主机
+zzzzzz
+
+### 启动zookeeper集群
+在每台主机上执行
+```
+zkServer.sh start
+zkServer.sh status
+```
+### 启动Storm
+在主节点上启动nimbus, ui, logview，在从节点上启动supervisor, logviewer
+```
+# Nimbus 进程启动：
+storm nimbus &
+# Supervisor 进程启动：
+storm supervisor &
+# UI 进程启动：
+storm ui &
+# Log Viewer 进程启动：
+storm logviewer &
+```
+
+### 启动kafka
+在主节点执行
+```
+bin/zookeeper-server-start.sh config/zookeeper.properties
+bin/kafka-server-start.sh config/server.properties
+```
+
+### 部署拓扑程序，监听kafka输入
+在主节点部署storm，还没做😵
+
+### 启动python脚本
+send.py
---
+++ send.py
@@ -0,0 +1,39 @@
+from kafka import KafkaProducer
+import json
+import time
+
+# Kafka server address
+bootstrap_servers = 'localhost:9092'
+
+# Create Kafka producer
+producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+
+# JSON array file path
+json_file = 'sample.json'
+
+# Topic to send messages to
+topic = 'json_data'
+
+# Read JSON file
+with open(json_file, 'r') as file:
+    data = json.load(file)
+
+# Number of records to send each time
+batch_size = 5
+
+# Interval between each batch of messages (seconds)
+interval = 5
+
+# Loop to send messages
+for i in range(0, len(data), batch_size):
+    batch = data[i:i+batch_size]  # Get records for the current batch
+    for item in batch:
+        # Send the record to the Kafka topic
+        producer.send(topic, json.dumps(item).encode('utf-8'))
+        print("Sent:", item)
+    # Flush and wait
+    producer.flush()
+    time.sleep(interval)
+
+# Close the Kafka producer connection
+producer.close()
